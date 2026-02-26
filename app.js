0c2a6956dd29b39c2fe1fc93dc59868b9bec381d
Alberto <alberto@MacBook-Air-de-Alberto.local>
2026-02-26 20:26:14 +0100

    Initial Stripe integration

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 require('dotenv').config();
-const stripe = require('stripe');
+
+const Stripe = require('stripe');
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
 var app = express();
 
@@ -13,7 +15,7 @@ app.engine('hbs', exphbs({
 }));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json({}));
 
 /**
@@ -27,26 +29,24 @@ app.get('/', function(req, res) {
  * Checkout route
  */
 app.get('/checkout', function(req, res) {
-  // Just hardcoding amounts here to avoid using a database
   const item = req.query.item;
   let title, amount, error;
 
   switch (item) {
     case '1':
-      title = "The Art of Doing Science and Engineering"
-      amount = 2300      
+      title = "The Art of Doing Science and Engineering";
+      amount = 2300;
       break;
     case '2':
-      title = "The Making of Prince of Persia: Journals 1985-1993"
-      amount = 2500
-      break;     
+      title = "The Making of Prince of Persia: Journals 1985-1993";
+      amount = 2500;
+      break;
     case '3':
-      title = "Working in Public: The Making and Maintenance of Open Source"
-      amount = 2800  
-      break;     
+      title = "Working in Public: The Making and Maintenance of Open Source";
+      amount = 2800;
+      break;
     default:
-      // Included in layout view, feel free to assign error
-      error = "No item selected"      
+      error = "No item selected";
       break;
   }
 
@@ -58,10 +58,57 @@ app.get('/checkout', function(req, res) {
 });
 
 /**
+ * Create PaymentIntent
+ */
+app.post('/create-payment-intent', async (req, res) => {
+  try {
+    const amount = req.body.amount;
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: 'usd',
+      automatic_payment_methods: { enabled: true }
+    });
+
+    res.json({
+      clientSecret: paymentIntent.client_secret
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      error: err.message
+    });
+  }
+});
+
+/**
  * Success route
  */
-app.get('/success', function(req, res) {
-  res.render('success');
+app.get('/success', async function(req, res) {
+  try {
+    const paymentIntentId = req.query.payment_intent;
+
+    if (!paymentIntentId) {
+      return res.render('success', {
+        error: "PaymentIntent not found"
+      });
+    }
+
+    const paymentIntent = await stripe.paymentIntents.retrieve(paymentIntentId);
+
+    res.render('success', {
+      amount: paymentIntent.amount / 100,
+      paymentIntentId: paymentIntent.id,
+      status: paymentIntent.status
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.render('success', {
+      error: err.message
+    });
+  }
 });
 
 /**
